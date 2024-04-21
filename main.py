20dfbfa2f34970a24b1b0b3ce106514e32493ff3
JGoodin-01 <jakegoodin.01@gmail.com>
2024-04-21 13:47:35 -0400

    Simplify Execution

--- main.py
+++ main.py
@@ -1,35 +1,52 @@
 import subprocess
-import os
+import pandas as pd
+from src.training.ExperimentRunner import ExperimentRunner
+from src.EXPERIMENT_CONFIGS import EXPERIMENT_CONFIGS
 
 
-def run_preprocessing():
-    """Run the data preprocessing script."""
-    subprocess.run(["python", "./scripts/run_preprocessing.py"], check=True)
+def run_script(script_path):
+    try:
+        with subprocess.Popen(
+            ["python", script_path],
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        ) as proc:
+            # Read line by line as it's being output
+            while True:
+                output = proc.stdout.readline()
+                if output == "" and proc.poll() is not None:
+                    break
+                if output:
+                    print(output.strip())
+            rc = proc.poll()
+            return rc
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error running script {script_path}: {e}")
 
 
-def train_and_evaluate():
-    """Run the training and evaluation script for a given model."""
-    subprocess.run(["python", "-m", "scripts.train_and_evaluate"], check=True)
+def run_training():
+    data = pd.read_csv("./data/processed_with_pca.csv").dropna(subset=["η / mPa s"])
+    X, y = data.drop("η / mPa s", axis=1), data["η / mPa s"].values
+    runner = ExperimentRunner(EXPERIMENT_CONFIGS)
 
+    feature_selection_strategies = [
+        {"name": "Base"},
+        {"name": "SelectKBest"},
+        # {"name": "RFE"},
+        # {"name": "BFS"},
+    ]
 
-def run_dataset_plotting():
-    """Run the data plotting script."""
-    subprocess.run(["python", "./scripts/run_dataset_plotting.py"], check=True)
-
-
-def run_pcas():
-    """Run the pca script."""
-    subprocess.run(["python", "./scripts/run_pcas.py"], check=True)
-
-
-def run_model_plotting():
-    """Run the data plotting script."""
-    subprocess.run(["python", "./scripts/run_model_plotting.py"], check=True)
+    runner.run_cross_experiment(X, y, feature_selection_strategies)
 
 
 if __name__ == "__main__":
-    # run_preprocessing()
-    run_pcas()
-    # run_dataset_plotting()
-    # train_and_evaluate()
-    # run_model_plotting()
+    # Run pre-training scripts
+    # run_script("./src/preprocessing/preprocessing.py")
+    run_script("./src/evaluation/dataset_plots.py")
+    run_script("./src/preprocessing/PCAs.py")
+
+    # Run training & evaluation
+    # run_training()
+    # run_script("./src/evaluation/model_plots.py")
